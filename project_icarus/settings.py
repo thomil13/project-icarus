06870f1a3d18737ab1ecdb02ee3291a067308aa7
thomil13 <thomas.milde13@gmail.com>
2025-11-27 11:31:33 +0000

    Preparation for deployment

--- project_icarus/settings.py
+++ project_icarus/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ovu74j!n1!6pelbzp8hn@c*1k@x5r71_4zn^r!%55$lbu+!4z-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
