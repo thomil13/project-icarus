9345af8ccb529b71a9a52f3703cc06655277438d
thomil13 <thomas.milde13@gmail.com>
2025-12-15 16:53:13 +0000

    add authentication

--- imagefeed/urls.py
+++ imagefeed/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ImagePostListView.as_view(), name='imagepost_list'),
+    path('<slug:slug>/', views.ImagePostListView.as_view(), name='imagepost_detail'),
 ]
